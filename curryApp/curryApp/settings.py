14bbd815781458ff71e70b7b59fdb6631a1ff86d
Shimbori Kosuke <ee194083@meiji.ac.jp>
2023-04-27 20:53:11 +0000

    key

--- curryApp/curryApp/settings.py
+++ curryApp/curryApp/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#^xbbv5*$@u85c!510g=qccm#s3hs0_txvhi7so&iniurziu=4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
